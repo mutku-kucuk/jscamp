ccea988174847f5e620dcf22a82bb5cf34784756
Muhammet Utku Küçük <muhammetutku@gmail.com>
2021-10-23 22:51:55 +0300

    first commit

---
+++ week1/js/javaScriptStarterKit/src/app.js
@@ -0,0 +1,29 @@
+/*console.log("Merhaba Kodlama.io")
+
+let dolarBugun = 9.20
+let dolarDun = 9.30
+
+{
+    let dolarDun = 9.10
+}
+
+console.log(dolarDun)
+
+const euroDun = 11.2
+euroDun = 11
+
+console.log(euroDun)*/
+
+
+//array
+//camelCasing
+//PascalCasing
+let konutKredileri = ["Konut Kredisi","Emlak Konut Kredisi","Özel Konut Kredisi"]
+//react
+console.log(konutKredileri)
+console.log("<ul>")
+for(let i = 0; i<konutKredileri.length; i++){
+    console.log("<li>"+konutKredileri[i]+"</li>")
+}
+console.log("</ul>")
+
